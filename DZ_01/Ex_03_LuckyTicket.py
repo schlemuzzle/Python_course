334459e72eefc75fd671332085a0a409be18610b
schlemuzzle <schlemuzzle@mail.ru>
2023-09-03 13:48:45 +0300

    Add Semi_02

--- DZ_01/Ex_03_LuckyTicket.py
+++ DZ_01/Ex_03_LuckyTicket.py
@@ -15,9 +15,3 @@ if (a + b + c == d + e + f):
     print("Yes")
 else:
     print("No")
-print(a)
-print(b)
-print(c)
-print(d)
-print(e)
-print(f)
---
+++ Semi_02/break, continue, walrus.py
@@ -0,0 +1,18 @@
+# i = 1
+# while i < 11:
+#     print(i, end=' ')
+# #    break
+#     i += 1
+# else:
+#     print('\n end!') # \n - переход на новую строку
+
+
+# i = 1
+# while i < 11:
+#     print(i, end=' ')
+#     i += 1
+# #    continue
+#     print('\nNO!')
+
+while (num:=int(input('-> '))) < 0:
+     print(f'Число {num} маленькое!')
