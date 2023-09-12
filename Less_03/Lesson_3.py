50e443cf3891c27c32f605ee3e4226b363e6c886
schlemuzzle <schlemuzzle@mail.ru>
2023-09-12 15:41:28 +0300

    Add Less_03

---
+++ Less_03/Lesson_3.py
@@ -0,0 +1,23 @@
+# def sumNumbers(n, y = 'Wassa!'):
+#     print(y)
+#     summa = 0
+#     for i in range(1, n + 1):
+#         summa += i
+#     return summa
+# n = int(input()) # 5
+# print(sumNumbers(n)) # 15
+# a = sumNumbers(2, 'Hello')
+# print(a)
+
+# def sum_str(*args):
+#     res = ''
+#     for i in args:
+#         res += i
+#     return res
+
+# print(sum_str('q', 'w', 'e',))
+# print(sum_str('q', 'w', 'e', 'r', 't', 'y'))
+
+import modules
+
+print(modules.max1(5, 9))
